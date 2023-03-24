841e67ac5f226a26553cf982a3b3a8012346df90
Lorne <dev@luotao.net>
2023-03-24 14:49:33 +1100

    refine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # from https://medium.com/@alexandre.tkint/integrate-openais-chatgpt-within-slack-a-step-by-step-approach-bea43400d311
+import logging
 
 from environs import Env
 
@@ -15,7 +16,7 @@ from slack_bolt import App
 SLACK_BOT_TOKEN = env.str("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = env.str("SLACK_APP_TOKEN")
 OPENAI_API_KEY = env.str("OPENAI_API_KEY")
-
+chatgpt_engine = "gpt-3.5-turbo"
 
 # Event API & Web API
 app = App(token=SLACK_BOT_TOKEN)
@@ -25,11 +26,11 @@ client = WebClient(SLACK_BOT_TOKEN)
 # This gets activated when the bot is tagged in a channel
 @app.event("app_mention")
 def handle_message_events(body, logger):
-    # Log message
-    print(str(body["event"]["text"]).split(">")[1])
-
     # Create prompt for ChatGPT
     prompt = str(body["event"]["text"]).split(">")[1]
+
+    # Log message
+    logging.info('Msg Received: '+prompt)
 
     # Let thre user know that we are busy with the request
     # response = client.chat_postMessage(channel=body["event"]["channel"],
@@ -39,17 +40,17 @@ def handle_message_events(body, logger):
     # Check ChatGPT
     openai.api_key = OPENAI_API_KEY
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine=chatgpt_engine,
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=4096,
         n=1,
         stop=None,
         temperature=0.5).choices[0].text
 
     # Reply to thread
-    response = client.chat_postMessage(channel=body["event"]["channel"],
+    api_response = client.chat_postMessage(channel=body["event"]["channel"],
                                        thread_ts=body["event"]["event_ts"],
-                                       text=f"Here you go: \n{response}")
+                                       text=response)
 
 
 if __name__ == "__main__":
